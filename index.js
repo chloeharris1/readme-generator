fa56685e354904352ea973646c1fd9bb8d3898dd
Chloe Harris <chloe.a.harris17@gmail.com>
2021-10-13 16:47:27 -0700

    Added DS_Store to gitignore, added required files and questions array to index.js

--- index.js
+++ index.js
@@ -1,13 +1,91 @@
-// TODO: Include packages needed for this application
+// Including packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input: Project Description, Installation, Usage, License, Contributing, Tests, and Questions
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your E-mail address',
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+      },
+    ]);
+  };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
